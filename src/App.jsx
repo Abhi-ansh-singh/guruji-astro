1737dec41b55d5dc1bcd00347a34a0db4737f537
Abhishek Singh <117518760+Abhi-ansh-singh@users.noreply.github.com>
2024-06-22 10:53:06 +0530

    Add files via upload
    Final Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { UserDetailsForm } from "./components";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100 p-4">
+      <UserDetailsForm />
+    </div>
+  );
+}
+
+export default App;
